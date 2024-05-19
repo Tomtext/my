b92e34e05ab8b472bd74a9e8e166c082d5c20312
Tomtext <3035236766@qq.com>
2024-05-19 16:11:30 +0800

    人工智能

--- net/人工智能/人工智能/人工智能.cpp
+++ net/人工智能/人工智能/人工智能.cpp
@@ -1,11 +1,45 @@
 ﻿#include <iostream>
 #include<fstream>
 #include<string>
+#include<assert.h>
 #include<Windows.h>
 #define cols 256
 using namespace std;
+bool isvalley(int a[cols][cols], int r, int c) {
+	assert(r >= 0 && r < cols);
+	assert(c >= 0 && c < cols);
+	if ((a[r][c] < a[r - 1][c]) && (a[r][c] < a[r + 1][c] )&& (a[r][c] < a[r][c - 1] )&& (a[r][c] < a[r][c + 1] )) {
+		return true;
+	}
+	return false;
+}
+void extremes(const int a[][cols], int r, int c) {
+	int max = a[0][0];
+	int min = a[0][0];
+	int pos[2][2] = { 0 };
+	for (int i = 0; i < r; ++i) {
+		for (int j = 0; j < c; ++j) {
+			if (max < a[i][j]) {
+				max = a[i][j];
+				pos[0][0] = i;
+				pos[0][1] = j;
+			}
+			if (min > a[i][j]) {
+				min = a[i][j];
+				pos[1][0] = i;
+				pos[1][1] = j;
+			}
+		}
+	}
+	cout << "最大值为" << max << endl; ;
+	printf("位置为%d %d", pos[0][0], pos[0][1]);
+	cout << "最小值为" << min << endl;
+	printf("位置为%d %d", pos[1][0], pos[1][1]);
+}
 bool ispeak(int a[cols][cols], int r, int c) {
-	if ((a[r][c] > a[r - 1][c]) && (a[r][c] > a[r + 1][c] )&& (a[r][c] > a[r][c - 1]) && (a[r][c] > a[r][c + 1])) {
+	assert(r>=0&&r<cols);
+	assert(c >= 0 && c < cols);
+	if ((a[r][c] > a[r - 1][c]) && (a[r][c] > a[r + 1][c]) && (a[r][c] > a[r][c - 1]) && (a[r][c] > a[r][c + 1])&&(a[r][c]>a[r-1][c-1])&&(a[r][c]>a[r+1][c+1])&&(a[r][c]>a[r+1][c-1])&&(a[r][c]>a[r-1][c+1])) {
 		return true;
 	}
 	return false;
@@ -33,14 +67,21 @@ int main()
 			file >> map[i][j];
 		}
 	}
+	int total_peaks = 0;
 	for (int i = 0; i < nrows; ++i) {
 		for (int j = 0; j < ncols; ++j) {
 			file >> map[i][j];
 			if (ispeak(map, i, j)) {
-				cout << "峰点出现在行:" << i << "列:" << j << endl;
+				cout << "峰点出现在:" << i<< "行" << j << "列" << endl;
+				total_peaks++;
+
+			}else if (isvalley(map, i, j)) {
+				cout << "谷点出现在:" << i <<"行"  << j <<"列" << endl;
 			}
 		}
 	}
+	extremes(map, nrows, ncols);
+	printf("有%d个峰点\n", total_peaks);
 	system("pause");
 	file.close();
 }
